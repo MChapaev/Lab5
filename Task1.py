ecf98f8f47f4f715a1285aa301225aa3dd41ad7d
MChapaev <magomed.chapaev1@mail.ru>
2025-05-11 20:55:26 +0300

    Add ip validation method based on regex

--- Task1.py
+++ Task1.py
@@ -1,3 +1,27 @@
+import re
+# Validation
+def is_valid_ipv4(ip: str) -> bool:
+    pattern = re.compile(r'''
+        ^
+        (?:
+          (?:25[0-5]|
+          2[0-4][0-9]|
+          1[0-9]{2}|
+          [1-9]?[0-9])
+          \.
+        ){3}
+        (?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])
+        $
+    ''', re.VERBOSE)
+    return bool(pattern.match(ip))
+
+
+def validate_ipv4(ip: str) -> str:
+    if not is_valid_ipv4(ip):
+        raise ValueError(f"Invalid IP-address: {ip}")
+    return ip
+
+
 # Main
 if __name__ == "__main__":
     print("Hello world")
